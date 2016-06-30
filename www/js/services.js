697bf396985001b4a231e0f8fcea92f59ec8f7b5
ccaseau <charlotte.caseau@gmail.com>
2016-06-30 09:45:02 +0200

    Message d'erreurs
    Gestion des principaux messages d'erreurs du formulaire. On empeche le
    remplissage de la bdd si des champs ne sont pas, ou sont mal remplis

--- www/js/services.js
+++ www/js/services.js
@@ -17,3 +17,41 @@ app.factory('ManageScore', function(){
     },
   }
 })
+
+//********************************************GESTION DE LA BASE DE DONNEE***************************************************//
+
+//Table Users
+app.factory('UsersDataService', function ($cordovaSQLite, $ionicPlatform) {
+
+  return {
+
+    //Retourner les utilisateurs déja présent dans la base de donnée
+    getAll: function(callback){
+         $ionicPlatform.ready(function () {
+           $cordovaSQLite.execute(db, 'SELECT * FROM Users').then(function (results) {
+             var data = []
+             for (i = 0, max = results.rows.length; i < max; i++) {
+               data.push(results.rows.item(i))
+             }
+             callback(data)
+           })
+        })
+      },
+
+    //Rajouter un utilisateur dans la base de donnée
+      createUser: function (user) {
+        return $cordovaSQLite.execute(db, 'INSERT INTO Users (mail,age,formation,code,tel,sexe,info) VALUES (?,?,?,?,?,?,?)', [user.mail, user.age, user.formation,user.code, user.tel,user.sexe,user.info])
+      },
+
+    //Supprimer un utilisateur
+    deleteUser: function(id){
+       return $cordovaSQLite.execute(db, 'DELETE FROM Users where id = ?', [id])
+     },
+
+     //Retourne un utilisateur avec une certaine adresse
+     getByMail: function(mail){
+        return $cordovaSQLite.execute(db, 'SELECT COUNT(*) FROM Users where mail = ?', [mail])
+
+       }
+  }
+})
